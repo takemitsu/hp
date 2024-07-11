dda1284e7a920deae04faeec08a6b3603229b8f7
Takemitsu <takemitsu@notespace.jp>
2024-07-11 11:21:25 +0900

    各画面 簡易版 light/dark theme 対応

---
+++ setTheme.mini.js
@@ -0,0 +1,7 @@
+const getStoredTheme = () => localStorage.getItem('theme');
+const setStoredTheme = theme => localStorage.setItem('theme', theme);
+
+function changeTheme(theme) {
+  document.documentElement.setAttribute("data-bs-theme", theme);
+  setStoredTheme(theme);
+}
